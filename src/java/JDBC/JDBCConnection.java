690c53033c91e7c8839c7113a3c2348240806f05
Kumaran Babu Kuttuva <mankank@amazon.com>
2018-12-16 09:51:40 +0530

    Adding JDBC program

--- src/java/JDBC/JDBCConnection.java
+++ src/java/JDBC/JDBCConnection.java
@@ -5,12 +5,29 @@
  */
 package JDBC;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author mankank
  */
 public class JDBCConnection {
-    public static void main(String[] args) {
-        
-    }
+        public static void main(String[] args) throws ClassNotFoundException, SQLException {
+                String query = "select * from logindetails";
+                Class.forName("com.mysql.jdbc.Driver"); //load the driver
+                Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3310/studentlogin","mankank","gmpwothah");
+                Statement statement = connection.createStatement();
+                ResultSet resultset = statement.executeQuery(query);
+                while (true) {
+                        resultset.next();
+                        System.out.println(resultset.getString(1) + " " + resultset.getString(2));
+                        if (resultset.last()) {
+                                break;
+                        }
+                }
+        }
 }
